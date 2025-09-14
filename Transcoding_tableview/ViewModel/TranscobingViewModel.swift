6813b70f6939e047e7fc577bd1a7c7b0883d81e0
TejasvSingh <singhtejasv98@gmail.com>
2025-09-14 10:01:32 -0500

    changed getDataFromServer Function

--- Transcoding_tableview/ViewModel/TranscobingViewModel.swift
+++ Transcoding_tableview/ViewModel/TranscobingViewModel.swift
@@ -1,27 +1,13 @@
-//
-//  TranscobingViewModel.swift
-//  Transcoding_tableview
-//
-//  Created by Koushik Reddy Kambham on 9/12/25.
-//
-
 import Foundation
 
 class TranscobingViewModel {
     var transcodingData: [Transcobing] = []
     var networkManager = NetworkManager.shared
+    var allTranscodings: [Transcobing] = []
         
     func getDataFromServer(closure: @escaping (() -> Void)) {
         networkManager.getData(from: ServerURL.baseURL.rawValue) { [weak self] result in
-            var allTranscodings: [Transcobing] = []
-            if let result = result {
-                for item in result {
-                    if let transcodings = item.transcodings {
-                        allTranscodings.append(contentsOf: transcodings)
-                    }
-                }
-            }
-            self?.transcodingData = allTranscodings
+            self?.transcodingData = result?.first?.transcodings ?? []
             closure()
         }
     }
